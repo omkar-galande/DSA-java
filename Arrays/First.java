bddd7b1132957493c86ab47eb99a7cf323bfe638
T E J A <galandeomkar07@gmail.com>
2026-07-29 18:08:54 +0530

    initial DSA commit

---
+++ Arrays/First.java
@@ -0,0 +1,8 @@
+public class First{
+    public static void main(String[] args) {
+        int[] arr =  
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(i);
+        }
+    }
+}gi
