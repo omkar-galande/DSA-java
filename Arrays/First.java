5acac5daa4ca2842fe6fd5299d6e035f697caab0
omkar-galande <galandeomkar@gmail.com>
2026-07-30 17:09:13 +0530

    Learned about string array

--- Arrays/First.java
+++ Arrays/First.java
@@ -1,8 +1,11 @@
 public class First{
     public static void main(String[] args) {
-        int[] arr =  
+        int[] arr = {1, 2, 3, 4};
         for (int i = 0; i < arr.length; i++){
             System.out.println(i);
         }
+        String[] string = new String[5];
+        System.out.println(string[0]);
+
     }
 }
---
+++ Arrays/StirngArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StirngArray {
+    static void main(String[] args) {
+        String[] arr = new String[9];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.next();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
