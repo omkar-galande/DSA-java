048bb80c2d17f14d1196f809021884d147693fc8
omkar-galande <galandeomkar@gmail.com>
2026-07-30 17:29:49 +0530

    Updated string

--- Arrays/StirngArray.java
+++ Arrays/StirngArray.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class StirngArray {
     static void main(String[] args) {
-        String[] arr = new String[9];
+        String[] arr = new String[5];
         Scanner sc = new Scanner(System.in);
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.next();
         }
+        System.out.println(Arrays.toString(arr));
+        arr[2] = "Galande";
         System.out.println(Arrays.toString(arr));
     }
 }
