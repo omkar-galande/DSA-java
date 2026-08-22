e6cdd93e6f7b9117cb41a6c52373c7ca0384931d
omkar-galande <galandeomkar07@gmail.com>
2026-08-22 22:33:36 +0530

    Binary Search

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,47 @@
+public class BinarySearch {
+    public static void main(String args[]){
+        int[] arr = {7, 6, 5, 4, 3, 2, 1};
+        int target = 5;
+        System.out.println(search2(arr, target));
+    }
+
+    static int search(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+     static int search2(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
